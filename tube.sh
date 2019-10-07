04b25de7524be00bfdb2e994504f349cfe9ac676
pro710 <36391922+pro710@users.noreply.github.com>
2019-10-07 14:29:30 +0000

    Add files via upload

---
+++ tube.sh
@@ -0,0 +1,24 @@
+termux-setup-storage
+apt update && apt upgrade -y
+pkg install python python2 ruby ffmpeg wget nano curl git -y
+gem install lolcat
+pip install youtube-dl
+mkdir /sdcard/Videos
+mkdir -p ~/.config/youtube-dl
+mkdir ~/bin
+wget https://pastebin.com/raw/SwhieysW -O ~/bin/termux-url-opener
+dos2unix ~/bin/termux-url-opener
+clear
+echo "Installation Succes" | lolcat
+echo "Set Up Finished" | lolcat
+echo "=====[ta3limat]=======" | lolcat
+echo "intiha2 tanzil, ma3lik ghir twajah application youtube" | lolcat
+echo "w t9om bi dokhol l2ay videos wcliki partager" | lolcat
+echo "mn ba3d ghadi takhtar app termux" | lolcat
+echo "wghadi tla3 lik page tanzil mp3 ola videos hd" | lolcat
+echo "ma3lik ila takhtar ra9m 3la hsaba jawda li baghi" | lolcat
+echo "w videos mobachara ghadi tal9ah fi memoire" | lolcat
+echo "bi dabt fi wahd dossier ismo Videos" | lolcat
+echo "=======ZA3IM======"
+echo "Pour more info, telegram @za3imPro"
+echo " GoooooD LucK "  | lolcat
